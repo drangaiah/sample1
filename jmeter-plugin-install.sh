1a3fc7c4e71161d159eeacf92fe89ae80ef74576
deeaar21 <99270464+deeaar21@users.noreply.github.com>
2022-02-08 23:25:05 +0000

    Update jmeter-plugin-install.sh

--- jmeter-plugin-install.sh
+++ jmeter-plugin-install.sh
@@ -9,9 +9,9 @@ echo ${JMETER_PLUGIN_MANAGER_VERSION}
 echo "Downloading Plugin Manager"
 curl -L https://jmeter-plugins.org/get/ --output ${JMETER_HOME}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar
 
-java -cp /opt/apache/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
+java -cp /opt/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
 
-cd /opt/apache/apache-jmeter-${JMETER_VERSION}/bin/
+cd /opt/apache-jmeter-${JMETER_VERSION}/bin/
 java -jar ${JMETER_HOME}/lib/cmdrunner-${JMETER_CMD_RUNNER_VERSION}.jar --tool org.jmeterplugins.repository.PluginManagerCMD install jpgc-udp=0.4
 java -jar ${JMETER_HOME}/lib/cmdrunner-${JMETER_CMD_RUNNER_VERSION}.jar --tool org.jmeterplugins.repository.PluginManagerCMD install jpgc-casutg=2.10
 
